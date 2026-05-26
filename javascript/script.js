581a2d38042a099d435f220033b495e0560fa89e
youssef <ENGyoussefel2005@gmail.com>
2026-05-27 00:41:29 +0300

    finish animations and responsive design

---
+++ javascript/script.js
@@ -0,0 +1,74 @@
+
+const elements = document.querySelectorAll('.animation');
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach((entry)=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add('show');
+
+        } else {
+
+            entry.target.classList.remove('show');
+
+        }
+
+    });
+
+});
+
+elements.forEach((el)=>{
+    observer.observe(el);
+});
+
+const questions = document.querySelectorAll(".ques");
+
+questions.forEach((q) => {
+    q.addEventListener("click", () => {
+        
+        // يقفل كل اللي فات
+        questions.forEach(item => {
+            if (item !== q) item.classList.remove("open");
+        });
+
+        // يفتح/يقفل الحالي
+        q.classList.toggle("open");
+    });
+});
+
+const menu = document.getElementById("menu");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menu.addEventListener("click", () => {
+    mobileMenu.classList.toggle("show");
+});
+
+const rows = document.querySelectorAll(".hidden-row");
+
+const rowObserver = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show-row");
+
+        }else{
+
+            entry.target.classList.remove("show-row");
+
+        }
+
+    });
+},{
+    threshold: 0.2
+});
+
+rows.forEach((row,index)=>{
+
+    row.style.transitionDelay = `${index * 0.2}s`;
+
+    rowObserver.observe(row);
+
+});
